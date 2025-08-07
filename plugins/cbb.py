03dc52cd542deb0c6c3f46614d456200dfab4b7c
shinubo28always <shinubo28always@gmail.com>
2025-08-07 22:23:30 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -37,18 +37,17 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             ])
         )
 
-    elif data == "start":
-        await query.message.edit_text(
-            text=START_MSG.format(first=query.from_user.first_name),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('Aɴɪᴍᴇs', url='https://t.me/+o2_AIqUz0adjY2M1'),
-                InlineKeyboardButton('Bᴀsᴇ', url='https://t.me/AniReal_Anime_Zone')],
-                [InlineKeyboardButton('• ᴀʙᴏᴜᴛ', url='https://telegra.ph/AniReal---Support-Bot-08-07'),
-                InlineKeyboardButton(' ʜᴇʟᴘ •', url='https://telegra.ph/AniReal---Support-Bot-08-07-3')],
-                [InlineKeyboardButton("Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url='https://t.me/AniReal_Updates')]
-            ]
-        )
+    await query.message.edit_text(
+        text=START_MSG.format(first=query.from_user.first_name),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton('Aɴɪᴍᴇs', url='https://t.me/+o2_AIqUz0adjY2M1'),
+             InlineKeyboardButton('Bᴀsᴇ', url='https://t.me/AniReal_Anime_Zone')],
+            [InlineKeyboardButton('• ᴀʙᴏᴜᴛ', url='https://telegra.ph/AniReal---Support-Bot-08-07'),
+             InlineKeyboardButton(' ʜᴇʟᴘ •', url='https://telegra.ph/AniReal---Support-Bot-08-07-3')],
+            [InlineKeyboardButton("Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url='https://t.me/AniReal_Updates')]
+        ])
+    )
 
     elif data == "close":
         await query.message.delete()
